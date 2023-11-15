496c55d219b931e04b49baeb9630e7400a2f6cd0
R0dney03 <150660749+R0dney03@users.noreply.github.com>
2023-11-15 17:07:05 +0300

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,17 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        String course = "DIT";
+        String name = "rog";
+        String reg = "22/06557";
+        String dob = "3/04/2027";
+        String vision = "to pass";
+        String favLang = "Java";
+
+        System.out.println("Course: " + course);
+        System.out.println("Name: " + name);
+        System.out.println("Registration Number: " + reg);
+        System.out.println("Date of Birth: " + dob);
+        System.out.println("Vision: " + vision);
+        System.out.println("Favourite Programming Language: " + favLang);
+    }
+}
